e7e0aafb291f07586a0de40205930d97b2cfb001
Kuddleman <edl1155@gmail.com>
2021-04-07 09:53:09 -0700

    add date methods

--- src/DateTimeDemonstration.java
+++ src/DateTimeDemonstration.java
@@ -1,2 +1,30 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class DateTimeDemonstration {
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+        System.out.println("Today is " + today.format(outputFormatter));
+
+        LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
+        System.out.println("Today is still " + today.format(outputFormatter));
+        System.out.println("Tomorrow is " + tomorrow.format(outputFormatter));
+
+        LocalDate christmas = LocalDate.of(2021, Month.DECEMBER, 25);
+        Period timeToXmas = Period.between(today, christmas);
+        System.out.println(timeToXmas);
+        System.out.println("Christmas is in " + timeToXmas.get(ChronoUnit.DAYS) + " days.");
+        // this only gives a remainder
+        System.out.println("Christmas is in " + timeToXmas.get(ChronoUnit.YEARS) +
+                " years and " + timeToXmas.get(ChronoUnit.MONTHS) + " months and " +
+                timeToXmas.get(ChronoUnit.DAYS) + " days.");
+        System.out.println("Christmas is in " + ChronoUnit.DAYS.between(today,christmas) + " days.");
+
+    }
 }
